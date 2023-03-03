fb9017024cc50a2cf2a4857ee4b4c11a7b06cd83
Andy-Kin3 <andylodme@gmail.com>
2023-03-03 09:19:21 +0100

    Created a few configuration files in a folder to move fluent API code to the various entities

---
+++ MatchStat.DataModel/EntityTypeConfigurartion/FieldsConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using MatchStat.DataModel.DataModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchStat.DataModel.EntityTypeConfigurartion
+{
+    public class FieldsConfiguration: IEntityTypeConfiguration<Fields>
+    {
+        public void Configure(EntityTypeBuilder<Fields> entity)
+        {
+            entity.ToTable("Field");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ MatchStat.DataModel/EntityTypeConfigurartion/TeamConfigurartion.cs
@@ -0,0 +1,21 @@
+﻿using MatchStat.DataModel.DataModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MatchStat.DataModel.EntityTypeConfigurartion
+{
+    public class TeamConfigurartion : IEntityTypeConfiguration<Team>
+    {
+        public void Configure(EntityTypeBuilder<Team> entity)
+        {
+            entity.ToTable("Team");
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Name)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+        }
+    }
+}
