b7a6df6c04fa20e8ef0cb5290a04e7dcf532ab1a
Titus Yusinyu <titusxp@outlook.com>
2023-03-11 13:29:17 +0100

    moved all windows to a separate folder

---
+++ MatchStat.UI/Windows/MatchesWindow2.cs
@@ -0,0 +1,10 @@
+﻿namespace MatchStat.UI.Windows
+{
+    public partial class MatchesWindow2 : Form
+    {
+        public MatchesWindow2()
+        {
+            InitializeComponent();
+        }
+    }
+}
