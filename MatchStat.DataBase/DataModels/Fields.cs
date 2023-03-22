884064ba602f285775a94e2ad777c59fd6399efe
Andy-Kin3 <49214978+Andy-Kin3@users.noreply.github.com>
2023-03-22 13:38:13 +0100

    Separated models from database

---
+++ MatchStat.DataBase/DataModels/Fields.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchStat.DataModel.DataModels;
+
+public partial class Fields
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+    public string Country { get; set; }
+
+    public virtual ICollection<Match> Matches { get; } = new List<Match>();
+}
---
+++ MatchStat.DataBase/DataModels/Tournament.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatchStat.DataModel.DataModels;
+
+public partial class Tournament
+{
+    public int Id { get; set; }
+
+    public string? Name { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    public virtual ICollection<Match> Matches { get; } = new List<Match>();
+}
